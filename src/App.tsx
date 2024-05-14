32d1d1ea84cd360104d60be681015e77bcbb13e6
dcDalin <30606041+dcDalin@users.noreply.github.com>
2024-05-14 21:25:15 +0300

    setup react-router-dom, draft screens and main layout

--- src/App.tsx
+++
@@ -1,5 +0,0 @@
-function App() {
-  return <h1 className="text-3xl font-bold underline">Todo App</h1>;
-}
-
-export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App.tsx';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import './index.css';
+import ErrorPage from './pages/error-page';
+import MainLayout from './layouts/main-layout';
+
+const router = createBrowserRouter([
+  {
+    element: <MainLayout />,
+    errorElement: <ErrorPage />,
+    children: [
+      {
+        path: '/',
+        element: <div>Login Screen</div>,
+      },
+      {
+        path: '/list',
+        element: <div>List Screen</div>,
+      },
+    ],
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
